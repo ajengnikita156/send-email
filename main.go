49a03324df0413aec68aff02afcfb38c472fa4b1
ajengnikita156 <21221753096@smkassalaambandung.sch.id>
2023-11-08 13:44:20 +0700

    nambah html

--- main.go
+++ main.go
@@ -12,8 +12,58 @@ func main() {
 
 	send.SetHeader("From", "ajengnikita14@gmail.com")
 	send.SetHeader("To", "ajengnktaa15@gmail.com")
-	send.SetHeader("Subject", "HAI KAMU")
-	send.SetBody("text/plain", "THIS IS BODY jbajbancahieaknab jbjaja")
+	send.SetHeader("Subject", "selamat...")
+	send.SetBody("text/html", 
+	`<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Job Acceptance</title>
+		<style>
+			body {
+				font-family: Arial, sans-serif;
+			}
+	
+			.container {
+				max-width: 400px;
+				margin: 0 auto;
+				padding: 20px;
+			}
+	
+			h2 {
+				color: #007BFF;
+			}
+	
+			p {
+				margin-top: 10px;
+			}
+	
+			.message {
+				background-color: #f0f0f0;
+				border: 1px solid #ccc;
+				padding: 10px;
+				margin-top: 20px;
+			}
+	
+			.signature {
+				margin-top: 20px;
+				font-weight: bold;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<h2>Job Acceptance</h2>
+			<p>Dear AJENG NIKITA ANGGRAENI ,</p>
+			<p>We are pleased to inform you that you have been accepted for the position of <strong>JOB TITTLE</strong> at our company. Your employment will begin on <strong>10-11-2023</strong>.</p>
+	
+			<div class="message">
+				<p>Congratulations on your new role, and we look forward to having you as part of our team!</p>
+			</div>
+	
+			<p class="signature">Sincerely,<br>PT. PERUSAHAN</p>
+		</div>
+	</body>
+	</html>`)
 
 	a := gomail.NewDialer("smtp.gmail.com", 587, "ajengnikita14@gmail.com", "fird jdwa rujm xlyq")
 	fmt.Println("Sudah Terkirim!")
@@ -24,40 +74,4 @@ func main() {
 	}
 }
 
-
-
-
-
-
-
-
-
-
-// func sendMailSimple() {
-// 	auth := smtp.PlainAuth(
-// 		"",
-// 		"ajengnikita14@gmail.com",
-// 		"ajeng1056",
-// 		"smtp.gmail.com",
-// 	)
-
 // 	// pw := "fird jdwa rujm xlyq"
-
-// 	msg := "Subject: My special subject\nThis is the body of the email."
-
-// 	err := smtp.SendMail(
-// 		"smtp.gmail.com:587",
-// 		auth,
-// 		"ajengnikita14@gmail.com",
-// 		[]string{"ajengnikita14@gmail.com"},
-// 		[]byte(msg),
-// 	)
-
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
-
-// func main() {
-// 	sendMailSimple()
-// }
